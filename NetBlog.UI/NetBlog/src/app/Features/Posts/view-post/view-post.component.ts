140521b5e64bcf4d5ce25f7f68c8201d554af017
KhilchukArtemIP-14 <89968135+KhilchukArtemIP-14@users.noreply.github.com>
2024-05-12 15:35:54 +0300

    Added view post page

---
+++ NetBlog.UI/NetBlog/src/app/Features/Posts/view-post/view-post.component.ts
@@ -0,0 +1,32 @@
+import {Component, OnInit} from '@angular/core';
+import {PostDTO} from "../models/PostDTO";
+import {PostsService} from "../services/PostService";
+import {ActivatedRoute} from "@angular/router";
+import {DatePipe, NgForOf, NgIf} from "@angular/common";
+
+@Component({
+  selector: 'app-view-post',
+  standalone: true,
+  imports: [
+    DatePipe,
+    NgForOf,
+    NgIf
+  ],
+  templateUrl: './view-post.component.html',
+  styleUrl: './view-post.component.css'
+})
+export class ViewPostComponent implements OnInit{
+  public post!:PostDTO;
+  public postId:string;
+  public constructor(private postService:PostsService,
+                     private route: ActivatedRoute,
+  ) {
+    this.postId = this.route.snapshot.params['id'];
+  }
+
+  ngOnInit(): void {
+      this.postService
+        .getPostById(this.postId)
+        .subscribe(data=> this.post=data)
+  }
+}
--- NetBlog.UI/NetBlog/src/app/app.routes.ts
+++ NetBlog.UI/NetBlog/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import {HomepageComponent} from "./Features/homepage/homepage.component";
 import {ViewPostsComponent} from "./Features/Posts/view-posts/view-posts.component";
+import {ViewPostComponent} from "./Features/Posts/view-post/view-post.component";
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path:"posts",
     component:ViewPostsComponent
+  },
+  {
+    path:"posts/:id",
+    component:ViewPostComponent
   }
 ];
