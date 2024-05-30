80753abaaac987cad7d445a92d020024dd5a2186
KhilchukArtemIP-14 <89968135+KhilchukArtemIP-14@users.noreply.github.com>
2024-05-30 17:10:52 +0300

    More pagination

--- NetBlog.UI/NetBlog/src/app/Features/Posts/view-recent-posts/view-recent-posts.component.ts
+++ NetBlog.UI/NetBlog/src/app/Features/Posts/view-recent-posts/view-recent-posts.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import {PostsService} from "../services/post-service";
 import {PostSummaryDTO} from "../models/PostSummaryDTO";
 import {DatePipe, NgForOf} from "@angular/common";
@@ -17,9 +17,30 @@ export class ViewRecentPostsComponent implements OnInit{
   public constructor(private postsService:PostsService) {
   }
   public posts:PostSummaryDTO[]=[]
+  private pageSize = 5;
+  private pageNumber = 1;
+  public isLoading = true;
+  public hasMore = true;
   ngOnInit(): void {
         this.postsService
           .getPostSummaries()
-          .subscribe(data=> this.posts=data)
+          .subscribe(data=>{
+            this.posts=data;
+          this.isLoading=false
+          } )
     }
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    const position = window.scrollY + window.innerHeight;
+    const height = document.documentElement.scrollHeight;
+    if (!this.isLoading && this.hasMore && position >= height) {
+      this.pageNumber++;
+      this.isLoading=true;
+      this.postsService.getPostSummaries(this.pageNumber,this.pageSize,null,false).subscribe(data=>{
+        this.hasMore = data.length!=0;
+        this.posts = this.posts.concat(data);
+        this.isLoading=false;
+      })
+    }
+  }
 }
