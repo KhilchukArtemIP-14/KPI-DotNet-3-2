3ef22d93f23ec2678123e09409daaecac9b7e531
KhilchukArtemIP-14 <89968135+KhilchukArtemIP-14@users.noreply.github.com>
2024-05-31 22:27:37 +0300

    Added attributes

--- NetBlog.API/NetBlog.BAL/DTO/RegisterUserDTO.cs
+++ NetBlog.API/NetBlog.BAL/DTO/RegisterUserDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,16 @@ namespace NetBlog.BAL.DTO
 {
     public class RegisterUserDTO
     {
+        [Required]
+        [MinLength(1)]
         public string Name { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [MinLength(6)]
         public string Password { get; set; }
+        [Required]
         public string[] Roles { get; set; }
     }
 }
--- NetBlog.API/NetBlog.BAL/DTO/UpdateUserDTO.cs
+++ NetBlog.API/NetBlog.BAL/DTO/UpdateUserDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,11 @@ namespace NetBlog.BAL.DTO
 {
     public class UpdateUserDTO
     {
+        [Required]
+        [MinLength(1)]
         public string Name { get; set; }
+        [Required]
+        [MinLength(1)]
         public string Bio {  get; set; }
     }
 }
